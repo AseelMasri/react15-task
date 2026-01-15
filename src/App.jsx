0ca40ec455e7f3aad1968b76614a2cd9a2e686c3
AseelMasri <masriaseel70@gmail.com>
2026-01-15 22:16:49 +0200

    update App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from './component/navbar/Navbar'
 import Hero from './component/hero/Hero'
 import About from './component/about/About'
 import ContactMe from './component/contactMe/ContactMe'
-import Footer from './component/footer/footer'
+import Footer from './component/footer/Footer'
 function App() {
   return (
     <>
