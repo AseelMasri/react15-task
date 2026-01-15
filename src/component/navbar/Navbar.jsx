e29c9bbc7332067156c5a1d63a2964b52b912a6d
AseelMasri <masriaseel70@gmail.com>
2026-01-15 22:34:31 +0200

    Update navbar component and styles

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import styles from "./navbar.module.css";
 
-function Navbar() {
+export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className={`navbar navbar-expand-lg navbar-dark fixed-top ${styles.nav}`}>
+      <div className="container">
 
-export default Navbar
+        {/* Logo */}
+        <a className={`navbar-brand ${styles.brand}`} href="#home">
+          Start Bootstrap
+        </a>
+
+        {/* Toggle Button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mainNav"
+          aria-controls="mainNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        {/* Menu */}
+        <div className="collapse navbar-collapse" id="mainNav">
+          <ul className="navbar-nav ms-auto gap-4">
+
+            <li className="nav-item">
+              <a
+                className={`nav-link text-uppercase fw-bold ${styles.link}`}
+                href="#portfolio"
+              >
+                Portfolio
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a
+                className={`nav-link text-uppercase fw-bold ${styles.link}`}
+                href="#about"
+              >
+                About
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a
+                className={`nav-link text-uppercase fw-bold ${styles.link}`}
+                href="#contact"
+              >
+                Contact
+              </a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
