eeb70318e3c0a7922ced667ba43674eab224a7d2
AseelMasri <masriaseel70@gmail.com>
2026-01-15 22:10:55 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './component/navbar/Navbar'
+import Hero from './component/hero/Hero'
+import About from './component/about/About'
+import ContactMe from './component/contactMe/ContactMe'
+import Footer from './component/footer/footer'
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <About />
+    <ContactMe />
+    <Footer />    
+    </>
+  )
+}
+
+export default App
---
+++ src/component/portfolio/Portfoilo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Portfoilo() {
+  return (
+    <div>Portfoilo</div>
+  )
+}
+
+export default Portfoilo
