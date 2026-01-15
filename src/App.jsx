9bfd7146ba07ba7fdba9c04b5d4e55c281c679a5
AseelMasri <masriaseel70@gmail.com>
2026-01-15 23:02:20 +0200

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Hero from './component/hero/Hero'
 import About from './component/about/About'
 import ContactMe from './component/contactMe/ContactMe'
 import Footer from './component/footer/Footer'
+import Portfoilo from './component/portfolio/Portfoilo'
 function App() {
   return (
     <>
     <Navbar />
     <Hero />
+    <Portfoilo />
     <About />
     <ContactMe />
     <Footer />    
